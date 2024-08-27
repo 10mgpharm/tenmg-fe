7b322cace5f36dc30f6cf9ef7d6e3250d98f89ea
Ahmed Olanrewaju <dev@consode.com>
2024-08-27 14:54:25 +0100

    feat: configure app theme

---
+++ commitlint.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 72],
+        'subject-case': [2, 'always', ['lower-case', 'camel-case', 'sentence-case']],
+        'type-enum': [2, 'always', [
+            'build',
+            'chore',
+            'ci',
+            'docs',
+            'feat',
+            'fix',
+            'perf',
+            'refactor',
+            'style',
+            'test'
+        ]],
+        // Add or override more rules as needed
+    },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@nextui-org/react";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Apply for 10MG Credit</Button>
+      <Button variant={'default'}>Apply for 10MG Credit</Button>
+      <Button variant={'destructive'}>Apply for 10MG Credit</Button>
+      <Button variant={'ghost'}>Apply for 10MG Credit</Button>
+      <Button variant={'link'}>Apply for 10MG Credit</Button>
+      <Button variant={'outline'}>Apply for 10MG Credit</Button>
+      <Button variant={'secondary'}>Apply for 10MG Credit</Button>
     </main>
   );
 }
