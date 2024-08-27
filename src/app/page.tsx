518e638e1496fb8acf7f89c20834727d3bb953ff
Ahmed Olanrewaju <dev@consode.com>
2024-08-27 15:57:40 +0100

    fix: update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant={'default'}>Apply for 10MG Credit</Button>
+      <Button
+        variant={'default'}>Apply for 10MG Credit</Button>
       <Button variant={'destructive'}>Apply for 10MG Credit</Button>
       <Button variant={'ghost'}>Apply for 10MG Credit</Button>
       <Button variant={'link'}>Apply for 10MG Credit</Button>
