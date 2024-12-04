497b25b4b2c9c104ed4c6f16a61042c213cf3142
Element27 <olasocials@gmail.com>
2024-12-04 17:36:41 +0100

    refactor: store front by category keep

---
+++ src/app/(protected)/storefront/category/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StoreProductCardComponent from '../components/StoreProductCardComponent'
+
+export default function StoreFrontByCategory() {
+  return (
+    <div>
+      <StoreProductCardComponent />
+    </div>
+  )
+}
