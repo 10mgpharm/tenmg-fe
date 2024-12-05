daa1996b3981c81a9a7c876d642b33ad0f0c2ab6
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-12-05 02:22:59 +0100

    fix: Transaction Page Empty State Design

--- src/app/(protected)/vendors/transactions-history/page.tsx
+++ src/app/(protected)/vendors/transactions-history/page.tsx
@@ -173,8 +173,8 @@ const TransactionHistory = () => {
           </Flex>
         ) : tableData.length === 0 ? (
           <EmptyResult
-            heading="Nothing to show here yet"
-            content="You don’t have any transaction history yet. When you do, they’ll appear here."
+            heading="No record available"
+            content=""
           />
         ) : (
           <TableContainer border="1px solid #F9FAFB" borderRadius="10px">
