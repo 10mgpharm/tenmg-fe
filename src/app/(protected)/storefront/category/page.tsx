989fb0401f9a13c2ca6939623465a6ca53911d16
Element27 <olasocials@gmail.com>
2024-12-05 09:59:14 +0100

    feat: product category screen and prodcut details screen

--- src/app/(protected)/storefront/category/page.tsx
+++ src/app/(protected)/storefront/category/page.tsx
@@ -1,10 +1,19 @@
+'use client'
 import React from 'react'
 import StoreProductCardComponent from '../components/StoreProductCardComponent'
 
 export default function StoreFrontByCategory() {
+
   return (
     <div>
-      <StoreProductCardComponent />
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6'>
+        {Array(20)
+          .fill(null)
+          .map((_, i: number) => (
+            // <p key={i}>Item {i}</p>
+            <StoreProductCardComponent key={1} />
+          ))}
+      </div>
     </div>
   )
 }
