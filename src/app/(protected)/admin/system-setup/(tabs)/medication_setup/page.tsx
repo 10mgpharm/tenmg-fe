060127b3480774a29dfb497a7ef352b552b1f19e
Ahmed Olanrewaju <dev@consode.com>
2024-12-07 09:24:00 +0100

    refactor: admin system setup and medication setup tab

--- src/app/(protected)/admin/system-setup/(tabs)/medication_setup/page.tsx
+++ src/app/(protected)/admin/system-setup/(tabs)/medication_setup/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function Page() {
     // redirect to initial tab page
-    redirect('system-setup/medication_setup');
+    redirect('medication_setup/brands');
 }
--- src/app/(protected)/admin/system-setup/layout.tsx
+++ src/app/(protected)/admin/system-setup/layout.tsx
@@ -1,17 +1,17 @@
 import { TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import React from 'react'
-import TabsNavigation from './_components/TabNavigation';
+import SystemSetupTabNavigation from './_components/SystemSetupTabNavigation';
 
-export default function AdminSettingsLayout({
+export default function AdminSystemSetupSettingsLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <div className="p-8">
-            <h2 className='text-2xl font-semibold text-gray-700 mb-2'>Settings</h2>
+            <h2 className='text-2xl font-semibold text-gray-700 mb-2'>System Setup</h2>
             <Tabs>
-                <TabsNavigation />
+                <SystemSetupTabNavigation />
                 <TabPanels>
                     <TabPanel>
                         {children}
