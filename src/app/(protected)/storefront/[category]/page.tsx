7f10d918801c1df278904df16a2a0e87dec69360
Element27 <olasocials@gmail.com>
2024-12-14 15:04:21 +0100

    feat:MGH-283: storefront and product by categories

---
+++ src/app/(protected)/storefront/[category]/page.tsx
@@ -0,0 +1,87 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import StoreProductCardComponent from '../_components/StoreProductCardComponent'
+// import EmptyProductScreen from '../_components/EmptyProductScreen'
+import BreadCrumbBanner from '../_components/BreadCrumbBanner'
+import { usePathname } from 'next/navigation'
+import requestClient from '@/lib/requestClient'
+import { useSession } from "next-auth/react";
+import { NextAuthUserSession } from '@/types'
+import EmptyProductScreen from '../_components/EmptyProductScreen'
+
+
+
+export default function StoreFrontByCategory() {
+
+  const session = useSession();
+  const userData = session.data as NextAuthUserSession;
+
+  const path = usePathname();
+
+  // console.log()
+
+  const splitPath = path.split('/');
+  console.log(splitPath);
+  const category = splitPath[splitPath.length - 1];
+  console.log(category);
+
+
+
+  const breadCrumb = [
+    {
+      text: 'Home',
+      link: '/'
+    },
+    {
+      text: 'Products',
+      link: '/storefront'
+    },
+    {
+      text: category,
+      link: '#'
+    }
+  ]
+
+  const [categoryData, setCategoryData] = useState(null);
+
+  useEffect(() => {
+
+    const fetchCategoryData = async () => {
+      try {
+        const data = await requestClient({ token: userData?.user?.token }).get(`/storefront/categories/${category}`);
+        console.log(data);
+        setCategoryData(data?.data?.data);
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchCategoryData();
+
+
+  }, [userData?.user?.token, category])
+
+  console.log("categoryData", categoryData);
+
+
+  return (
+    <section className="">
+      <BreadCrumbBanner breadCrumbsData={breadCrumb} />
+      {/* <section className=""> */}
+      {/* <EmptyProductScreen /> */}
+
+      <div className=" w-11/12 my-8 mx-auto">
+        <div className={`gap-x-4 gap-y-6 w-full   ${categoryData?.data.length > 0 ? "grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4" : " "}`}>
+          {categoryData?.data && categoryData?.data.length > 0 ? categoryData?.data
+            .map((data: any, i: number) => (
+              // <p key={i}>Item {i}</p>
+              <StoreProductCardComponent key={i} product={data} />
+            ))
+
+            : <EmptyProductScreen />
+          }
+        </div>
+      </div>
+    </section>
+    // </section>
+  )
+}
--- src/app/(protected)/storefront/page.tsx
+++ src/app/(protected)/storefront/page.tsx
@@ -3,10 +3,12 @@ import { signOut, useSession } from "next-auth/react";
 import { NextAuthUserSession } from "@/types";
 import Carousel from "./_components/Carousel";
 import ProductField from "./_components/ProductField";
+import { useEffect, useState } from "react";
+import requestClient from "@/lib/requestClient";
 
 const StoreFront = () => {
   const session = useSession();
-  const data = session.data as NextAuthUserSession;
+  const userData = session.data as NextAuthUserSession;
 
   const productTitle = [
     "FREQUENTLY BOUGHT ITEMS",
@@ -17,13 +19,38 @@ const StoreFront = () => {
     "HOSPITALS AND CLINICS",
   ];
 
+  // console.log(userData?.user?.token);
+  //  const response = await requestClient().post("/storefront"
+
+  //   const response = await requestClient({token: sessionData.user.token}).patch(
+  //     "account/settings/profile",
+  //     {
+  //         ...value
+  //     }
+  // )
+  const [storeFrontData, setStoreFrontData] = useState([]);
+  useEffect(() => {
+    const fetchStoreFront = async () => {
+      try {
+        const data = await requestClient({ token: userData?.user?.token }).get("/storefront");
+        // console.log(data);
+        setStoreFrontData(data?.data?.data);
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchStoreFront();
+  }, [userData?.user?.token])
+
+  // console.log("storeFrontData", storeFrontData);
+
   return (
     <div className="">
       <div className="p-8 px-6 md:px-20 max-w-screen-2xl mx-auto">
         <Carousel />
       </div>
-      {productTitle.map((title, i) => (
-        <ProductField key={i} title={title} />
+      {storeFrontData?.map((category, i) => (
+        category?.products?.length > 0 && <ProductField key={i} category={category} />
       ))}
     </div>
   );
