0dd9b532b47209bdffd2604a9a9c1e82d52226ba
sakinlade <sakinlade@vatebra.com>
2024-12-17 15:27:45 +0100

    fix: qa comment

--- src/app/(protected)/admin/products/[snug]/page.tsx
+++ src/app/(protected)/admin/products/[snug]/page.tsx
@@ -86,6 +86,60 @@ const ProductDetail = ({params}: any ) => {
                 <ExploreData />
             </div>
         </div>
+        {/* <div className="mt-5">
+            <h2 className='text-xl font-semibold mb-4'>Global Pentazocine</h2>
+            <div className="flex gap-5 mb-3">
+                <div className="">
+                    <Image src={pill} alt='' className='w-[380px] h-auto'/>
+                    <div className="grid grid-cols-3 gap-x-2 gap-y-6 mt-3">
+                        <div className="space-y-2">
+                            <p className='text-gray-600 text-sm'>Name</p>
+                            <p className='text-gray-700 font-medium'>Zimax</p>
+                        </div>
+                        <div className="space-y-2">
+                            <p className='text-gray-600 text-sm'>Brand</p>
+                            <p className='text-gray-700 font-medium'>Azithromycin</p>
+                        </div>
+                        <div className="space-y-2">
+                            <p className='text-gray-600 text-sm'>Weight</p>
+                            <p className='text-gray-700 font-medium'>500mg</p>
+                        </div>
+                        <div className="space-y-2">
+                            <p className='text-gray-600 text-sm'>Category</p>
+                            <p className='text-gray-700 font-medium'>Tablet</p>
+                        </div>
+                        <div className="space-y-2">
+                            <p className='text-gray-600 text-sm'>Manufacturer</p>
+                            <p className='text-gray-700 font-medium'>Healthcare</p>
+                        </div>
+                        <div className="space-y-2">
+                            <p className='text-gray-600 text-sm'>Expire Date</p>
+                            <p className='text-gray-700 font-medium'>19/12/2024</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1 bg-white p-5 rounded-md">
+                    <Tabs>
+                        <TabList>
+                            <Tab>Product Essentials</Tab>
+                            <Tab>Product Statistics</Tab>
+                            <Tab>Product Reviews</Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <Essentials />
+                            </TabPanel>
+                            <TabPanel>
+                                <Statistics />
+                            </TabPanel>
+                            <TabPanel>
+                                <Reviews />
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </div>
+            </div>
+        </div> */}
     </div>
   )
 }
