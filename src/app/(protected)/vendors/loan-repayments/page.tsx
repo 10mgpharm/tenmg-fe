3021ea2c3963f7d5db5606fe5e707c33678b18f5
sakinlade <sakinlade@vatebra.com>
2025-01-02 16:59:54 +0100

    feat: loan repayment

---
+++ src/app/(protected)/vendors/loan-repayments/page.tsx
@@ -0,0 +1,10 @@
+
+const LoanRepayments = () => {
+  return (
+    <div className="p-8">
+        <h3 className="font-semibold text-2xl">Loan Repayment</h3>
+    </div>
+  )
+}
+
+export default LoanRepayments
