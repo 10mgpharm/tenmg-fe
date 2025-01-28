dd08f9d9ad3d8de154ad5d00bef3b43842af4681
Element27 <olasocials@gmail.com>
2025-01-28 08:39:22 +0100

    bug:storefront

--- src/app/(protected)/storefront/_components/StoreProductCardComponent.tsx
+++ src/app/(protected)/storefront/_components/StoreProductCardComponent.tsx
@@ -36,7 +36,7 @@ export default function StoreProductCardComponent({ product }: any) {
 
   useEffect(() => {
     if (cartList) {
-      setAddedToCart(cartList?.items.findIndex((item) => item?.product.id === product?.id) >= 0)
+      setAddedToCart(cartList?.items?.findIndex((item) => item?.product?.id === product?.id) >= 0)
     }
   }, [cartList, product])
 
@@ -78,7 +78,7 @@ export default function StoreProductCardComponent({ product }: any) {
 
   const renderProductImage = () => (
     <div
-      style={{ backgroundImage: `url(${product.thumbnailFile})` }}
+      style={{ backgroundImage: `url(${product?.thumbnailFile})` }}
       className="w-[279px] h-[186px] bg-gray-50 bg-cover bg-center bg-no-repeat rounded-sm shadow-sm overflow-hidden"
     />
   );
@@ -86,19 +86,19 @@ export default function StoreProductCardComponent({ product }: any) {
   const renderProductDetails = () => (
     <div className="w-full">
       <div className="flex items-center justify-between my-2">
-        <p className="text-gray-950 font-semibold text-sm capitalize">{product.name} {product?.variation?.strengthValue}{product?.measurement?.name}</p>
+        <p className="text-gray-950 font-semibold text-sm capitalize">{product?.name} {product?.variation?.strengthValue}{product?.measurement?.name}</p>
         <div className="relative z-10 cursor-pointer" onClick={(e) => handleWishlistClick(e, product?.id)}>
           <HeartIcon className={`w-6 stroke-primary ${addedToWishlist ? "fill-primary-500" : "fill-primary-50"}`} />
         </div>
       </div>
       <div className="relative flex items-center gap-x-2">
-        {product.discountPrice > 0 && (
+        {product?.discountPrice > 0 && (
           <p className="text-gray-900 font-semibold my-2 text-sm">
-            ₦{parseInt(product.actualPrice) - parseInt(product.discountPrice)}
+            ₦{parseInt(product?.actualPrice) - parseInt(product?.discountPrice)}
           </p>
         )}
-        <p className={`font-semibold my-2 text-sm ${product.discountPrice > 0 ? "text-gray-400 line-through" : "text-gray-900"}`}>
-          ₦{product.actualPrice}
+        <p className={`font-semibold my-2 text-sm ${product?.discountPrice > 0 ? "text-gray-400 line-through" : "text-gray-900"}`}>
+          ₦{product?.actualPrice}
         </p>
       </div>
       <div className="flex items-center justify-between">
@@ -111,10 +111,10 @@ export default function StoreProductCardComponent({ product }: any) {
           size="sm"
           ml="1"
           borderRadius="full"
-          color={product.inventory.toLowerCase() === "in stock" ? "green.500" : "error.500"}
-          bgColor={product.inventory.toLowerCase() === "in stock" ? "green.100" : "error.100"}
+          color={product?.inventory?.toLowerCase() === "in stock" ? "green.500" : "error.500"}
+          bgColor={product?.inventory?.toLowerCase() === "in stock" ? "green.100" : "error.100"}
         >
-          <TagLabel>{product.inventory.toLowerCase()}</TagLabel>
+          <TagLabel>{product?.inventory?.toLowerCase()}</TagLabel>
         </Tag>
       </div>
     </div>
@@ -123,10 +123,10 @@ export default function StoreProductCardComponent({ product }: any) {
   const renderButtons = () => (
     <div className="flex items-center justify-between w-full gap-4 my-2">
       <button
-        disabled={!isProductClickable || product.inventory.toLowerCase() !== "in stock"}
+        disabled={!isProductClickable || product?.inventory?.toLowerCase() !== "in stock"}
         className="bg-primary-500 text-white w-full py-2 rounded-md text-xs mt-3 font-semibold cursor-pointer disabled:cursor-not-allowed"
         onClick={() => {
-          handleAddToCart(product.id, "add");
+          handleAddToCart(product?.id, "add");
           router.push("/storefront/checkout");
         }}
       >
@@ -135,7 +135,7 @@ export default function StoreProductCardComponent({ product }: any) {
       <button
         disabled={!isProductClickable || updateLoading}
         className="border border-primary-500 text-primary-500 w-full py-2 rounded-md cursor-pointer text-xs mt-3 font-semibold disabled:cursor-not-allowed"
-        onClick={() => { handleAddToCart(product.id, addedTocart ? "remove" : "add") }}
+        onClick={() => { handleAddToCart(product?.id, addedTocart ? "remove" : "add") }}
       >
         {addedTocart ? "Remove From Cart" : "Add To Cart"}
       </button>
@@ -146,7 +146,7 @@ export default function StoreProductCardComponent({ product }: any) {
     <div className="w-fit max-w-[311px] px-3 py-3 flex flex-col items-center justify-center shadow-lg rounded-md">
       {isProductClickable ? (
         <div>
-          <Link href={`/storefront/product/${product.slug}`}>
+          <Link href={`/storefront/product/${product?.slug}`}>
             {renderProductImage()}
           </Link>
           {renderProductDetails()}
