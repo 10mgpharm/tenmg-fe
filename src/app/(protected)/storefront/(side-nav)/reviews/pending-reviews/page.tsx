6c656a22cfb14e62b5f002838d5a0b99cc8d0a3b
Element27 <olasocials@gmail.com>
2025-03-10 09:34:31 +0100

    fix: audti lot search and review started

--- src/app/(protected)/storefront/(side-nav)/reviews/pending-reviews/page.tsx
+++ src/app/(protected)/storefront/(side-nav)/reviews/pending-reviews/page.tsx
@@ -1,13 +1,50 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import ReviewsCardComponent from '../../../_components/(reviews-component)/ReviewsCardComponent'
 import { temp_order } from '../../orders/temporders'
 import Image from 'next/image'
+import { useCartStore } from '../../../storeFrontState/useCartStore'
+import { useOrdersStore } from '../../../storeFrontState/useMyOrders'
+import { useSession } from 'next-auth/react'
+import { NextAuthUserSession } from '@/types'
 
-export default function page() {
+export default function Page() {
+
+  const session = useSession();
+  const userData = session.data as NextAuthUserSession;
+  const { cart } = useCartStore();
+
+  const { fetchOrders, loading, completedOrders } = useOrdersStore();
+
+  useEffect(() => {
+
+    if (userData?.user?.token) {
+
+      fetchOrders(userData?.user?.token);
+    }
+  }, [userData?.user?.token, fetchOrders])
+
+
+  const [products, setProducts] = useState([]);
+  // console.log(completedOrders)
+  useEffect(() => {
+    const extractedProducts = completedOrders
+      .flatMap(order => order.items?.map(item => item.product) || [])
+      .filter(product => product);
+
+    setProducts(prevProducts => {
+      if (JSON.stringify(prevProducts) !== JSON.stringify(extractedProducts)) {
+        return extractedProducts;
+      }
+      return prevProducts;
+    });
+  }, [completedOrders])
+
+
   return (
     <div className='w-full'>
-      {temp_order.slice(0, 3).map((item) => (
-        <ReviewsCardComponent key={item?.order_id} product={item} />
+      {products?.map((item) => (
+        <ReviewsCardComponent key={item?.id} product={item} />
       ))}
 
       {/* <div className='flex flex-col items-center justify-center w-full h-fit py-32'>
