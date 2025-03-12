3b510245dc4e64e7a7e788d76545b7ad83ed456a
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-03-12 06:02:34 +0100

    fix: disable access to vendor settings

--- src/app/(protected)/vendors/settings/_components/TabNavigation.tsx
+++ src/app/(protected)/vendors/settings/_components/TabNavigation.tsx
@@ -1,44 +1,72 @@
-'use client';
+"use client";
 
-import { Tabs, TabList, Tab, Text } from '@chakra-ui/react';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import { Tabs, TabList, Tab, Text } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { BusinessStatus } from "@/constants";
 
 const tabs = [
-    { label: 'General Settings', href: '/vendors/settings/general_settings' },
-    { label: 'Business Information', href: '/vendors/settings/business_information' },
-    { label: 'Notification', href: '/vendors/settings/notifications' },
-    { label: 'License Upload', href: '/vendors/settings/license_upload' },
-    { label: 'Team Members', href: '/vendors/settings/team_members' },
-    { label: 'API Key and Web-hooks', href: '/vendors/settings/api_keys' },
+  { label: "General Settings", href: "/vendors/settings/general_settings" },
+  {
+    label: "Business Information",
+    href: "/vendors/settings/business_information",
+  },
+  { label: "Notification", href: "/vendors/settings/notifications" },
+  { label: "License Upload", href: "/vendors/settings/license_upload" },
+  { label: "Team Members", href: "/vendors/settings/team_members" },
+  { label: "API Key and Web-hooks", href: "/vendors/settings/api_keys" },
 ];
 
-export default function TabsNavigation() {
-    const pathname = usePathname();
-    const activeIndex = tabs.findIndex((tab) => pathname.startsWith(tab.href));
+const mustAlwaysBeEnabled = (name: string) =>
+  ["General Settings", "Business Information", "Notification"].includes(name);
 
-    return (
-        <Tabs index={activeIndex} variant="unstyled">
-            <TabList overflow={"auto"}>
-                {tabs.map((tab, index) => (
-                    <Tab
-                        key={index}
-                        as={Link}
-                        href={tab.href}
-                        _selected={{
-                            color: "primary.500",
-                            bg: "primary.50",
-                            borderRadius: "10px",
-                        }}
-                    >
-                        <div className="flex items-center gap-3">
-                            <Text fontSize={{ base: "xs", md: "sm" }}>
-                                {tab.label}
-                            </Text>
-                        </div>
-                    </Tab>
-                ))}
-            </TabList>
-        </Tabs>
-    );
+const isLinkDisabled = (businessStatus: string, name: string) => {
+  const disabledBusinessStatuses = [
+    BusinessStatus.PENDING_VERIFICATION,
+    BusinessStatus.PENDING_APPROVAL,
+    BusinessStatus.REJECTED,
+    BusinessStatus.LICENSE_EXPIRED,
+    BusinessStatus.SUSPENDED,
+    BusinessStatus.BANNED,
+  ];
+  return disabledBusinessStatuses.includes(businessStatus as BusinessStatus) &&
+    !mustAlwaysBeEnabled(name)
+    ? true
+    : false;
+};
+
+export default function TabsNavigation({
+  businessStatus,
+}: {
+  businessStatus: string;
+}) {
+  const pathname = usePathname();
+  const activeIndex = tabs.findIndex((tab) => pathname.startsWith(tab.href));
+
+  return (
+    <Tabs index={activeIndex} variant="unstyled">
+      <TabList overflow={"auto"}>
+        {tabs.map((tab, index) => {
+          const disabled = isLinkDisabled(businessStatus, tab.label);
+          return (
+            <Tab
+              key={index}
+              as={Link}
+              href={disabled ? '#' : tab.href}
+              isDisabled={disabled}
+              _selected={{
+                color: "primary.500",
+                bg: "primary.50",
+                borderRadius: "10px",
+              }}
+            >
+              <div className="flex items-center gap-3">
+                <Text fontSize={{ base: "xs", md: "sm" }}>{tab.label}</Text>
+              </div>
+            </Tab>
+          );
+        })}
+      </TabList>
+    </Tabs>
+  );
 }
--- src/app/(protected)/vendors/settings/layout.tsx
+++ src/app/(protected)/vendors/settings/layout.tsx
@@ -1,12 +1,17 @@
 import { Divider, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import React from 'react'
 import TabsNavigation from './_components/TabNavigation'
+import { NextAuthUserSession } from '@/types';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
-export default function VendorSettingsLayout({
+export default async function VendorSettingsLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const session: NextAuthUserSession = await getServerSession(authOptions);
   return (
       <div className="p-4 md:p-8">
           <h2 className="font-semibold text-2xl text-gray-600">Settings</h2>
@@ -16,7 +21,7 @@ export default function VendorSettingsLayout({
           <Divider my={[2, 5]} border="1px solid gray.200" />
           <div className="mt-2 md:mt-4">
               <Tabs>
-                  <TabsNavigation />
+                  <TabsNavigation businessStatus={session?.user?.businessStatus} />
                   <Divider my={[2, 5]} border="1px solid gray.200" />
                   <TabPanels>
                       <TabPanel px={0}>
