0c07c2a535cb26766a223f3e10b878ac1c384a17
ugonna-onyejekwe <innocentugonna063@gmail.com>
2025-03-19 17:22:07 +0100

    feat: design admin wallets

--- src/app/(protected)/admin/wallet/_components/OverviewCard.tsx
+++ src/app/(protected)/admin/wallet/_components/OverviewCard.tsx
@@ -1,23 +1,40 @@
-import { classNames } from "@/utils"
-import Image from "next/image"
+import { classNames } from "@/utils";
+import Image from "next/image";
 
-const OverviewCard = (
-    {title, value, fromColor, toColor, image}: 
-    {title: string, value: string, fromColor: string, toColor: string, image: string}
-) => {
+const OverviewCard = ({
+  title,
+  value,
+  fromColor,
+  toColor,
+  image,
+}: {
+  title: string;
+  value: string;
+  fromColor: string;
+  toColor: string;
+  image: string;
+}) => {
   return (
-    <div className={classNames(`bg-gradient-to-r ${fromColor} ${toColor} rounded-lg relative`)}>
-    <div className="flex items-center justify-between py-14 px-6">
+    <div
+      className={classNames(
+        `bg-gradient-to-r ${fromColor} ${toColor} rounded-lg relative`
+      )}
+    >
+      <div className="flex items-center justify-between py-8 sm:py-12  lg:py-14 px-6">
         <div className="">
-            <p className="text-white text-lg font-medium mb-1">{title}</p>
-            <p className="font-bold text-white text-3xl">{value}</p>
+          <p className="text-white text-[15px] lg:text-lg font-medium mb-1">
+            {title}
+          </p>
+          <p className="font-bold text-white text-[18px] sm:text-[20px] lg:text-3xl">
+            {value}
+          </p>
         </div>
+      </div>
+      <div className="absolute top-3 inset-x-0 mx-auto">
+        <Image src={image} alt="" className="mx-auto" />
+      </div>
     </div>
-        <div className="absolute top-3 inset-x-0 mx-auto">
-            <Image src={image} alt="" className="mx-auto"/>
-        </div>
-    </div>
-  )
-}
+  );
+};
 
-export default OverviewCard
+export default OverviewCard;
