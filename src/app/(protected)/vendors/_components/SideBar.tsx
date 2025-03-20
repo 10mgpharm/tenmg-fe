d4f681567a34b046d091757fbf0bfa68d4c53583
ugonna-onyejekwe <innocentugonna063@gmail.com>
2025-03-20 04:43:04 +0100

    fix: edit

--- src/app/(protected)/vendors/_components/SideBar.tsx
+++ src/app/(protected)/vendors/_components/SideBar.tsx
@@ -25,6 +25,7 @@ import { useBreakpointValue } from "@chakra-ui/react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import Link from "next/link";
 import { BiMessageDetail } from "react-icons/bi";
+import { TbWallet } from "react-icons/tb";
 
 const navSections = [
   {
@@ -35,6 +36,11 @@ const navSections = [
         name: "Customers Management",
         href: "/vendors/customers-management",
         icon: SlPeople,
+      },
+      {
+        name: "Wallets",
+        href: "/vendors/wallets",
+        icon: TbWallet,
       },
     ],
   },
