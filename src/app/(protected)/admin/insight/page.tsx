9f834a0b2ee31b035e60beaff691c18ffde1ac87
sakinlade <sakinlade@vatebra.com>
2025-03-21 12:04:36 +0100

    feat: new changes

--- src/app/(protected)/admin/insight/page.tsx
+++ src/app/(protected)/admin/insight/page.tsx
@@ -1,7 +1,44 @@
+"use client";
+
 import { Checkbox } from '@chakra-ui/react'
 import EmptyCard from '../../suppliers/_components/EmptyCard'
+import { useSession } from 'next-auth/react';
+import { NextAuthUserSession } from '@/types';
+import { useCallback, useEffect, useState } from 'react';
+import requestClient from '@/lib/requestClient';
 
 const Insight = () => {
+
+    const session = useSession();
+    const sessionData = session?.data as NextAuthUserSession;
+    const token = sessionData?.user?.token;
+
+    const [data, setData] = useState<any>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchOverview = useCallback(async () => {
+        setLoading(true);
+        try {
+            let query = `/admin/insights`;
+            
+            const response = await requestClient({ token: token }).get(query);
+            if (response.status === 200) {
+                setData(response.data.data);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if(!token) return;
+        fetchOverview();
+    }, [token, fetchOverview]);
+
+    console.log(data)
+
   return (
     <div className='p-8 min-h-[calc(100vh-108px)]'>
         <h2 className='text-2xl font-semibold text-gray-700'>Insight</h2>
