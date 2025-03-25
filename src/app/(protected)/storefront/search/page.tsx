ad2635c417b061a8f6c42a5392cb57726dbf9406
ugonna-onyejekwe <innocentugonna063@gmail.com>
2025-03-25 08:43:06 +0100

    fix: edit search product

--- src/app/(protected)/storefront/search/page.tsx
+++ src/app/(protected)/storefront/search/page.tsx
@@ -1,19 +1,18 @@
-'use client'
-import { Select, useRangeSlider, } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { Select, useRangeSlider } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 import {
   RangeSlider,
   RangeSliderTrack,
   RangeSliderFilledTrack,
   RangeSliderThumb,
-} from '@chakra-ui/react'
-import BreadCrumbBanner from '../_components/BreadCrumbBanner';
-import StoreProductCardComponent from '../_components/StoreProductCardComponent';
-import EmptyProductScreen from '../_components/EmptyProductScreen';
+} from "@chakra-ui/react";
+import BreadCrumbBanner from "../_components/BreadCrumbBanner";
+import StoreProductCardComponent from "../_components/StoreProductCardComponent";
+import EmptyProductScreen from "../_components/EmptyProductScreen";
 
 export default function SearchPage() {
-
   const min = 0;
   const max = 100;
   let defaultValue = [0, 30];
@@ -21,59 +20,65 @@ export default function SearchPage() {
   const [value, setVal] = useState(defaultValue);
 
   const onChangeEnd = (val) => {
-    console.log(val)
-    defaultValue = val
-    setVal(val)
-  }
+    console.log(val);
+    defaultValue = val;
+    setVal(val);
+  };
 
   // const [] = useState();
 
-
   const breadCrumb = [
     {
-      text: 'Home',
-      link: '/'
+      text: "Home",
+      link: "/",
     },
     {
-      text: 'Products',
-      link: '/storefront'
+      text: "Products",
+      link: "/storefront",
     },
     {
-      text: 'Search Result for Paracetamol',
-      link: '#'
-    }
-  ]
-
+      text: "Search Result for Paracetamol",
+      link: "#",
+    },
+  ];
 
   return (
     <section>
       <BreadCrumbBanner breadCrumbsData={breadCrumb} />
       <div className="grid grid-cols-4 gap-6  w-11/12 mx-auto my-10">
-        <div className='col-span-1'>
-          <div className='space-y-4 border-x border-gray-200 px-4 h-full'>
-            <Select placeholder='Categories '>
+        <div className="col-span-1 sticky top-[120px] h-[90vh]">
+          <div className="space-y-4 border-x border-gray-200 px-4 h-full">
+            <Select placeholder="Categories ">
               {/* <option value=""></option> */}
             </Select>
-            <Select placeholder='Brands'>
+            <Select placeholder="Brands">
               {/* <option value=""></option> */}
             </Select>
-            <Select placeholder='Availability '>
+            <Select placeholder="Availability ">
               {/* <option value=""></option> */}
             </Select>
-            <Select placeholder='Filter '>
+            <Select placeholder="Filter ">
               {/* <option value=""></option> */}
             </Select>
 
-            <div className='border-gray-200 border rounded-md  p-8 pt-4'>
-              <RangeSlider defaultValue={defaultValue} min={min} max={max} onChangeEnd={(val) => onChangeEnd(val)}>
+            <div className="border-gray-200 border rounded-md  p-8 pt-4">
+              <RangeSlider
+                defaultValue={defaultValue}
+                min={min}
+                max={max}
+                onChangeEnd={(val) => onChangeEnd(val)}
+              >
                 <RangeSliderTrack>
                   <RangeSliderFilledTrack />
                 </RangeSliderTrack>
-                <RangeSliderThumb index={0} ><div className='absolute top-4'>{value[0]}</div></RangeSliderThumb>
-                <RangeSliderThumb index={1}><div className='absolute top-4'>{value[1]}</div></RangeSliderThumb>
+                <RangeSliderThumb index={0}>
+                  <div className="absolute top-4">{value[0]}</div>
+                </RangeSliderThumb>
+                <RangeSliderThumb index={1}>
+                  <div className="absolute top-4">{value[1]}</div>
+                </RangeSliderThumb>
               </RangeSlider>
             </div>
-
           </div>
         </div>
 
@@ -88,13 +93,7 @@ export default function SearchPage() {
           </div> */}
           <EmptyProductScreen />
         </div>
-
       </div>
-
-
     </section>
-  )
+  );
 }
-
-
-
