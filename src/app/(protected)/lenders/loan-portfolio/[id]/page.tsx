359e09f2c40c2019a91084d150c734b351df1084
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-04-05 10:34:49 +0100

    feat: add lender loan details

--- src/app/(protected)/lenders/loan-portfolio/[id]/page.tsx
+++ src/app/(protected)/lenders/loan-portfolio/[id]/page.tsx
@@ -34,6 +34,7 @@ import { ColumsRepaymentFN } from "@/app/(protected)/admin/loan-repayment/_compo
 import { useSession } from "next-auth/react";
 import { LoanData, NextAuthUserSession } from "@/types";
 import requestClient from "@/lib/requestClient";
+import Loader from "@/app/(protected)/admin/_components/Loader";
 
 const theme = extendTheme({
   components: {
@@ -123,124 +124,135 @@ const Page = ({ params }: { params: { id: string } }) => {
         <ArrowLeft className="w-5 h-auto text-gray-600" />
         <Text>Back</Text>
       </Flex>
-      <Stack mt={4} gap={0.5}>
-        <Text fontSize={"1.2rem"} fontWeight={600}>
-          {data?.customer?.name}&apos;s Loan Information
-        </Text>
-        <Text fontSize={"15px"} color={"gray.600"}>
-          At a glance summary of {data?.customer?.name}&apos;s Loan.
-        </Text>
-      </Stack>
-      <Stack mt={4} className="p-5 rounded-md border bg-white">
-        <Flex justify={"space-between"} mb={3}>
-          <Stack>
-            <Text fontSize={"15px"} color={"gray.500"}>
-              Loan Amount
+      {loading && <Loader />}
+
+      {!loading && (
+        <>
+          <Stack mt={4} gap={0.5}>
+            <Text fontSize={"1.2rem"} fontWeight={600}>
+              {data?.customer?.name}&apos;s Loan Information
             </Text>
-            <Text fontWeight={500} fontSize={"1.2rem"}>
-              ₦{data?.totalAmount}
+            <Text fontSize={"15px"} color={"gray.600"}>
+              At a glance summary of {data?.customer?.name}&apos;s Loan.
             </Text>
           </Stack>
-          <Stack>
-            <Text fontSize={"15px"} color={"gray.500"}>
-              Loan Interest
-            </Text>
-            <Text fontWeight={500} fontSize={"1.2rem"}>
-              ₦{data?.interestAmount}
-            </Text>
+          <Stack mt={4} className="p-5 rounded-md border bg-white">
+            <Flex justify={"space-between"} mb={3}>
+              <Stack>
+                <Text fontSize={"15px"} color={"gray.500"}>
+                  Loan Amount
+                </Text>
+                <Text fontWeight={500} fontSize={"1.2rem"}>
+                  ₦{data?.totalAmount}
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize={"15px"} color={"gray.500"}>
+                  Loan Interest
+                </Text>
+                <Text fontWeight={500} fontSize={"1.2rem"}>
+                  ₦{data?.interestAmount}
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize={"15px"} color={"gray.500"}>
+                  Total Repayment
+                </Text>
+                <Text fontWeight={500} fontSize={"1.2rem"}>
+                  ₦{data?.interestAmount}
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize={"15px"} color={"gray.500"}>
+                  Duration
+                </Text>
+                <Text fontWeight={500} fontSize={"1.2rem"}>
+                  {data?.application?.durationInMonths} months
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize={"15px"} color={"gray.500"}>
+                  Start Date
+                </Text>
+                <Text fontWeight={500} fontSize={"1.2rem"}>
+                  1st of July, 2023
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize={"15px"} color={"gray.500"}>
+                  End Date
+                </Text>
+                <Text fontWeight={500} fontSize={"1.2rem"}>
+                  1st of July, 2023
+                </Text>
+              </Stack>
+            </Flex>
+            <Divider />
+            <Stack mt={3} maxW={"60%"}>
+              <Text fontWeight={600} fontSize={"1rem"} mb={2}>
+                Status
+              </Text>
+              <ThemeProvider theme={theme}>
+                <Progress h={"34px"} value={70} pos={"relative"}>
+                  <chakra.span className="text-white text-center text-sm absolute top-2 inset-x-0">
+                    Repayment in Progress - 75% Complete
+                  </chakra.span>
+                </Progress>
+              </ThemeProvider>
+            </Stack>
           </Stack>
-          <Stack>
-            <Text fontSize={"15px"} color={"gray.500"}>
-              Total Repayment
+          <Stack mt={6}>
+            <Text fontWeight={600} fontSize={"1.2rem"} mb={2}>
+              Repayment History
             </Text>
-            <Text fontWeight={500} fontSize={"1.2rem"}>
-              ₦ 123,849,900
-            </Text>
-          </Stack>
-          <Stack>
-            <Text fontSize={"15px"} color={"gray.500"}>
-              Duration
-            </Text>
-            <Text fontWeight={500} fontSize={"1.2rem"}>
-              1 year
-            </Text>
-          </Stack>
-          <Stack>
-            <Text fontSize={"15px"} color={"gray.500"}>
-              Start Date
-            </Text>
-            <Text fontWeight={500} fontSize={"1.2rem"}>
-              1st of July, 2023
-            </Text>
-          </Stack>
-          <Stack>
-            <Text fontSize={"15px"} color={"gray.500"}>
-              End Date
-            </Text>
-            <Text fontWeight={500} fontSize={"1.2rem"}>
-              1st of July, 2023
-            </Text>
-          </Stack>
-        </Flex>
-        <Divider />
-        <Stack mt={3} maxW={"60%"}>
-          <Text fontWeight={600} fontSize={"1rem"} mb={2}>
-            Status
-          </Text>
-          <ThemeProvider theme={theme}>
-            <Progress h={"34px"} value={70} pos={"relative"}>
-              <chakra.span className="text-white text-center text-sm absolute top-2 inset-x-0">
-                Repayment in Progress - 75% Complete
-              </chakra.span>
-            </Progress>
-          </ThemeProvider>
-        </Stack>
-      </Stack>
-      <Stack mt={6}>
-        <Text fontWeight={600} fontSize={"1.2rem"} mb={2}>
-          Repayment History
-        </Text>
-        <div className="">
-          {repaymentData?.length === 0 ? (
-            <EmptyOrder
-              heading={`No repayment history Yet`}
-              content={`You currently have no repayment history. All repayment history will appear here.`}
-            />
-          ) : (
-            <TableContainer border={"1px solid #F9FAFB"} borderRadius={"10px"}>
-              <Table>
-                <Thead bg={"#F2F4F7"}>
-                  {table?.getHeaderGroups()?.map((headerGroup) => (
-                    <Tr key={headerGroup.id}>
-                      {headerGroup.headers?.map((header) => (
-                        <Th textTransform={"initial"} px="0px" key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
+            <div className="">
+              {repaymentData?.length === 0 ? (
+                <EmptyOrder
+                  heading={`No repayment history Yet`}
+                  content={`You currently have no repayment history. All repayment history will appear here.`}
+                />
+              ) : (
+                <TableContainer
+                  border={"1px solid #F9FAFB"}
+                  borderRadius={"10px"}
+                >
+                  <Table>
+                    <Thead bg={"#F2F4F7"}>
+                      {table?.getHeaderGroups()?.map((headerGroup) => (
+                        <Tr key={headerGroup.id}>
+                          {headerGroup.headers?.map((header) => (
+                            <Th
+                              textTransform={"initial"}
+                              px="0px"
+                              key={header.id}
+                            >
+                              {header.isPlaceholder
+                                ? null
+                                : flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext()
+                                  )}
+                            </Th>
+                          ))}
+                        </Tr>
+                      ))}
+                    </Thead>
+                    <Tbody bg={"white"} color="#606060" fontSize={"14px"}>
+                      {table?.getRowModel()?.rows?.map((row) => (
+                        <Tr key={row.id}>
+                          {row.getVisibleCells()?.map((cell) => (
+                            <Td key={cell.id} px="0px">
+                              {flexRender(
+                                cell.column.columnDef.cell,
+                                cell.getContext()
                               )}
-                        </Th>
+                            </Td>
+                          ))}
+                        </Tr>
                       ))}
-                    </Tr>
-                  ))}
-                </Thead>
-                <Tbody bg={"white"} color="#606060" fontSize={"14px"}>
-                  {table?.getRowModel()?.rows?.map((row) => (
-                    <Tr key={row.id}>
-                      {row.getVisibleCells()?.map((cell) => (
-                        <Td key={cell.id} px="0px">
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </Td>
-                      ))}
-                    </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-              {/* <HStack mt={5} justify={"space-between"}>
+                    </Tbody>
+                  </Table>
+                  {/* <HStack mt={5} justify={"space-between"}>
                         <Flex alignItems={"center"} gap={2}>
                             <FaChevronLeft className='text-gray-500' />
                             <Text className='text-gray-500'>Prev</Text>
@@ -254,10 +266,12 @@ const Page = ({ params }: { params: { id: string } }) => {
                             <FaChevronRight className='text-gray-500' />
                         </Flex>
                     </HStack> */}
-            </TableContainer>
-          )}
-        </div>
-      </Stack>
+                </TableContainer>
+              )}
+            </div>
+          </Stack>
+        </>
+      )}
     </div>
   );
 };
