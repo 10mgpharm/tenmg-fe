229234e8b1250a084c1f637ad7e1e30b69ed452c
abosedeyahyah <abosedeyahyah@gmail.com>
2025-04-28 13:32:12 +0100

    fix: worked on supplier

---
+++ src/app/(protected)/suppliers/settings/(tabs)/shipping_address/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ShippingAddress from '../../_components/ShippingAddress'
+
+export default function ShippingAddressPage() {
+  return( 
+  <ShippingAddress />
+);
+}
