9b206109113de090ca50103ac0f2bbb0fd93c51f
Ugonna Onyejekwe <Ugonna.Onyejekwe@whytelabeltech.com>
2025-05-27 10:50:38 +0100

    fix: vendor audit logs

--- src/app/(protected)/vendors/audit-logs/page.tsx
+++ src/app/(protected)/vendors/audit-logs/page.tsx
@@ -1,77 +1,61 @@
 "use client";
-import { Button, Divider, Flex, HStack, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 import ActivityLogs from "./_components/ActivityLogs";
 import ApiLogs from "./_components/ApiLogs";
 import Webhook from "./_components/Webhook";
-import { CSVLink } from "react-csv";
-import { useState } from "react";
 
 const AuditLogs = () => {
+  return (
+    <div className="p-8">
+      <HStack justifyContent={"space-between"}>
+        <h3 className="font-semibold text-2xl">Audit Logs</h3>
+      </HStack>
+      <Divider my={2} />
+      <Tabs>
+        <TabList borderBottom={"0"}>
+          <Tab _selected={{ color: "blue.600", bg: "blue.50" }}>
+            <div className="px-8 rounded-sm">
+              <p className="text-sm">Activity Logs</p>
+            </div>
+          </Tab>
+          <Tab _selected={{ color: "blue.600", bg: "blue.50" }}>
+            <div className="px-8 rounded-sm">
+              <p className="text-sm text-gray-500">API Logs</p>
+            </div>
+          </Tab>
+          <Tab _selected={{ color: "blue.600", bg: "blue.50" }}>
+            <div className="px-8 rounded-sm">
+              <p className="text-sm text-gray-500">Webhook Logs</p>
+            </div>
+          </Tab>
+        </TabList>
+        <Divider mt={3} />
+        <TabPanels>
+          <TabPanel>
+            <ActivityLogs />
+          </TabPanel>
+          <TabPanel>
+            <ApiLogs />
+          </TabPanel>
+          <TabPanel>
+            <Webhook />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <Flex></Flex>
+      <Divider my={2} />
+    </div>
+  );
+};
 
-    const [auditLogCSV, setAuditLogCSV] = useState<any>([{}])
-    const headers = [
-        { label: "User", key: "actor.name" },
-        { label: "Action", key: "properties.action" },
-        { label: "Description", key: "description" },
-        { label: "Timestamp", key: "createdAt" },
-    ];
-    
-    return (
-        <div className="p-8">
-            <HStack justifyContent={"space-between"}>
-                <h3 className="font-semibold text-2xl">Audit Logs</h3>
-
-                <Button
-                    h={"38px"}
-                    variant={"outline"}
-                    px={3}
-                    fontSize={"14px"}
-                    color={"gray.500"}
-                    border={"1px solid #D0D5DD"}
-                >
-                    <CSVLink data={auditLogCSV} headers={headers} filename={"audit-logs.csv"} target="_blank">
-                        Export Data
-                    </CSVLink>
-                </Button>
-            </HStack>
-            <Divider my={2} />
-            <Tabs>
-                <TabList borderBottom={"0"}>
-                    <Tab _selected={{ color: 'blue.600', bg: 'blue.50' }}>
-                        <div className="px-8 rounded-sm">
-                            <p className="text-sm">Activity Logs</p>
-                        </div>
-                    </Tab>
-                    <Tab _selected={{ color: 'blue.600', bg: 'blue.50' }}>
-                        <div className="px-8 rounded-sm">
-                            <p className="text-sm text-gray-500">API Logs</p>
-                        </div>
-                    </Tab>
-                    <Tab _selected={{ color: 'blue.600', bg: 'blue.50' }}>
-                        <div className="px-8 rounded-sm">
-                            <p className="text-sm text-gray-500">Webhook Logs</p>
-                        </div>
-                    </Tab>
-                </TabList>
-                <Divider mt={3} />
-                <TabPanels>
-                    <TabPanel>
-                        <ActivityLogs setAuditLogCSV={setAuditLogCSV} />
-                    </TabPanel>
-                    <TabPanel>
-                        <ApiLogs />
-                    </TabPanel>
-                    <TabPanel>
-                        <Webhook />
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
-            <Flex>
-            </Flex>
-            <Divider my={2} />
-
-        </div>
-    )
-}
-
-export default AuditLogs
+export default AuditLogs;
