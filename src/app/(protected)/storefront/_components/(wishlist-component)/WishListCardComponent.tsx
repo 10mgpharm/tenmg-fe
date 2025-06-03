ce12ece93ca5a21d5209b7991959fcb1d84b2c91
Ugonna Onyejekwe <Ugonna.Onyejekwe@whytelabeltech.com>
2025-06-03 13:51:40 +0100

    feat: added view details model for wishlist - storefront

--- src/app/(protected)/storefront/_components/(wishlist-component)/WishListCardComponent.tsx
+++ src/app/(protected)/storefront/_components/(wishlist-component)/WishListCardComponent.tsx
@@ -1,14 +1,25 @@
-import { Badge, Button, Divider } from "@chakra-ui/react";
+import {
+  Badge,
+  Button,
+  Divider,
+  Spinner,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { useCartStore } from "../../(NoSideMenu)/storeFrontState/useCartStore";
 import { useWishlistStore } from "../../(NoSideMenu)/storeFrontState/useWIshlist";
+import DetailsModal, { WhishListProductType } from "./DetailsModal";
 
 export default function WishListCardComponent({ product, token }) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const [selectedProduct, setSelectedProduct] =
+    useState<WhishListProductType | null>(null);
+
   const router = useRouter();
   // when user clicks checkout, the item is added to cart
   const { addToCart, updateLoading } = useCartStore();
-  const handleAddToCart = (productId: string) => {
+  const handleAddToCart = (productId: number) => {
     const data = {
       productId,
       qty: 1,
@@ -17,30 +28,40 @@ export default function WishListCardComponent({ product, token }) {
     addToCart(data, token);
   };
 
-  const { addToWishlist, wishlist, loading, removeWishlistItem } =
-    useWishlistStore();
+  const { removeWishlistItem } = useWishlistStore();
 
-  // removeWishlistItem(wishlistId, userData?.user?.token);
+  const handleClick = (e) => {
+    // If a button was clicked (or a child of a button), don't open modal
+    if (e.target.closest("button")) return;
+
+    // Otherwise, open the modal
+    onOpen();
+    setSelectedProduct(product?.product);
+  };
 
   return (
-    <div className="m-4 border border-gray-200 rounded-md p-4">
-      <div>
-        <div className="grid grid-cols-1 lg:grid-cols-5 justify-between items-center lg:gap-5">
-          <div className="flex items-center gap-2 lg:col-span-4 col-span-1">
+    <>
+      <div
+        onClick={handleClick}
+        className="m-4 border border-gray-200 rounded-md p-3 hover:bg-primary/5 cursor-pointer"
+      >
+        <div className="flex items-center gap-4 md:gap-8 max-sm:flex-col max-sm:items-start max-sm:gap-2">
+          <div className="flex items-center gap-2 flex-1">
             <div
               style={{ backgroundImage: "url('/assets/images/pillImage.png')" }}
-              className="size-16 min-h-16 min-w-16 bg-cover bg-center bg-no-repeat"
+              className="size-16 min-h-16 min-w-16 bg-cover bg-center bg-no-repeat rounded-md"
             />
             <div>
               <h4 className="text-lg font-medium text-gray-700">
                 {product?.product?.name}
               </h4>
-              <p className="text-sm  text-gray-500 my-1">
+              <p className="text-sm  text-gray-500  line-clamp-2 max-w-md">
                 {product?.product?.description}
               </p>
             </div>
           </div>
-          <div className="space-y-4 col-span-1">
+
+          <div className="space-y-2 w-[200px]">
             <div className="flex items-center gap-3">
               {product?.product?.discountPrice > 0 && (
                 <p className="text-gray-900 font-semibold my-2 text-sm">
@@ -59,7 +80,7 @@ export default function WishListCardComponent({ product, token }) {
                 ₦{product?.product?.actualPrice}
               </p>
             </div>
-            <div className="space-x-4">
+            <div className="gap-4 flex">
               <Button
                 className=""
                 variant={"outline"}
@@ -85,6 +106,16 @@ export default function WishListCardComponent({ product, token }) {
           </div>
         </div>
       </div>
-    </div>
+
+      <DetailsModal
+        isOpen={isOpen}
+        onClose={onClose}
+        productData={selectedProduct}
+        addToCart={handleAddToCart}
+        removeWishlistItem={removeWishlistItem}
+        disableButton={updateLoading}
+        token={token}
+      />
+    </>
   );
 }
