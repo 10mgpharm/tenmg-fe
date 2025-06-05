5e3dd67cad43c4ffc274da7a6b7dc6b09ad4a9ec
Element27 <olasocials@gmail.com>
2025-06-05 10:01:55 +0100

    fix: text correction on delete modal

--- src/app/(protected)/admin/system-setup/_components/DeleteMedication.tsx
+++ src/app/(protected)/admin/system-setup/_components/DeleteMedication.tsx
@@ -5,26 +5,26 @@ import { Button, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 
 const DeleteMedication = (
-  { isOpen, onClose, handleDelete, title, isLoading}: 
-  { isOpen: boolean; onClose: () => void; handleDelete: () => void, title: string, isLoading: boolean}
+  { isOpen, onClose, handleDelete, title, isLoading }:
+    { isOpen: boolean; onClose: () => void; handleDelete: () => void, title: string, isLoading: boolean }
 ) => {
   return (
     <ModalComponent isOpen={isOpen} onClose={onClose}>
       <Image src={success} alt='' className='mx-auto mb-2' />
       <Text fontWeight={"semibold"} textAlign={"center"} fontSize={"x-large"}>Remove {title}</Text>
-      <Text color={"gray.500"} textAlign={"center"} mt={2}>Are your sure you want to remove this {title}?.</Text>
+      <Text color={"gray.500"} textAlign={"center"} mt={2}>Are you sure you want to remove this {title}?</Text>
       <div className="my-6 flex flex-col gap-2">
         <Button
-        isLoading={isLoading}
-        disabled={isLoading}
-        loadingText={"Deleting..."}
-        onClick={handleDelete} 
-        bg={"red.500"}
-        color={"white"}
-        _hover={{
-          background: "red"
-        }}
-        rounded={"md"}
+          isLoading={isLoading}
+          disabled={isLoading}
+          loadingText={"Deleting..."}
+          onClick={handleDelete}
+          bg={"red.500"}
+          color={"white"}
+          _hover={{
+            background: "red"
+          }}
+          rounded={"md"}
         >
           Yes, Remove
         </Button>
