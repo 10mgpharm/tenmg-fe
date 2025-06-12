c47a7ba787cf57df01cc5dd9db2d5f310a3eac99
Ugonna Onyejekwe <Ugonna.Onyejekwe@whytelabeltech.com>
2025-06-12 14:54:19 +0100

    fix: edited payment details history - admin

--- src/app/(protected)/admin/wallet/_components/TransactionDetail.tsx
+++ src/app/(protected)/admin/wallet/_components/TransactionDetail.tsx
@@ -29,7 +29,6 @@ const TransactionDetails = ({
   orderId?: any;
   selectedRow?: any;
 }) => {
- 
   return (
     <Drawer isOpen={isOpen} placement="right" onClose={onClose} size={"md"}>
       <DrawerOverlay />
@@ -61,73 +60,87 @@ const TransactionDetails = ({
             </Stack>
           )}
           <Stack gap={4} mt={4} px={3} py={5} rounded={"md"} className="border">
-            {
-              (type === "transaction" || type === "loan-wallet" || type === "supplier-wallet") ? (
-                <Flex justify={"space-between"}>
-                  <Text>Transaction Type</Text>
-                  <Tag 
+            {type === "transaction" ||
+            type === "loan-wallet" ||
+            type === "supplier-wallet" ? (
+              <Flex justify={"space-between"}>
+                <Text>Transaction Type</Text>
+                <Tag
                   colorScheme={
-                    (selectedRow?.txnType === "CREDIT" || selectedRow?.type === "CREDIT" )
-                    ? "green" : 
-                    selectedRow?.txnType === "DEBIT" || selectedRow?.type === "DEBIT"
-                    ? "red" : "orange"} fontWeight={500}
-                  >
-                    {(type === "transaction" || type === "supplier-wallet") ? selectedRow?.status : selectedRow?.type}
-                  </Tag>
-                </Flex>
-              )
-              : (
-                <Flex justify={"space-between"}>
-                  <Text>Status</Text>
-                  <Tag 
-                  colorScheme={
-                    (selectedRow?.txnType === "CREDIT" || selectedRow?.status === "success" )
-                    ? "green" : 
-                    selectedRow?.txnType === "DEBIT" 
-                    ? "red" : "orange"} fontWeight={500}
-                  >
-                    {
-                    selectedRow?.status === "CREDIT" 
-                    ? "PAID" 
-                    : selectedRow?.status === "DEBIT" 
-                    ? "NOT PAID" : 
-                    selectedRow?.order?.status === "PROCESSING" 
-                    ? "PENDING" 
-                    : selectedRow?.order?.status
-                    }
-                  </Tag>
-                </Flex>
-              )
-            }
-            {
-              type !== "sup_payout" && (
-                <Flex justify={"space-between"}>
-                  <Text>Transaction Description</Text>
-                  <Text fontWeight={500}>
-                    {(selectedRow?.txnGroup === "DEBIT_ON_ORDER_CANCELLATION" || selectedRow?.txnGroup === "DEBIT_COMMISSION_ON_ORDER_CANCELLATION") 
-                    ? "Order Cancelled" 
-                    : selectedRow?.txnGroup === "CREDIT_ON_ORDER_COMPLETION" ? "Order Completed" :
-                    selectedRow?.txnGroup === "CREDIT_COMMISSION_ON_ORDER_COMPLETION" ? "Commission On Order Completed" :
-                    selectedRow?.txnGroup === "WITHDRAW_TO_BANK" ? "Withdraw" :
-                    selectedRow?.transactionGroup === "payout" ? "Payout" :
-                    selectedRow?.transactionGroup === "repayment_interest" ? "Payment Interest" :
-                    selectedRow?.transactionGroup === "loan_interest" ? "Loan Interest" :
-                    selectedRow?.transactionGroup === "loan_disbursement" ? "Loan Disbursement" :
-                    ""
+                    selectedRow?.txnType === "CREDIT" ||
+                    selectedRow?.type === "CREDIT"
+                      ? "green"
+                      : selectedRow?.txnType === "DEBIT" ||
+                        selectedRow?.type === "DEBIT"
+                      ? "red"
+                      : "orange"
                   }
-                  </Text>
-                </Flex>
-              )
-            }
-            {
-              orderId && (
-                <Flex justify={"space-between"}>
-                  <Text>OrderID</Text>
-                  <Text fontWeight={500}>
-                    {/* {type === "sup_payout" ? (selectedRow?.id ?? "N/A") : type === "loan-wallet" ? (selectedRow?.identifier ?? "N/A") : (selectedRow?.order?.identifier ?? "N/A")} */}
-                    {orderId}
-                  </Text>
-                </Flex>
+                  fontWeight={500}
+                >
+                  {type === "transaction" || type === "supplier-wallet"
+                    ? selectedRow?.status
+                    : selectedRow?.type}
+                </Tag>
+              </Flex>
+            ) : (
+              <Flex justify={"space-between"}>
+                <Text>Status</Text>
+                <Tag
+                  colorScheme={
+                    selectedRow?.txnType === "CREDIT" ||
+                    selectedRow?.status === "success"
+                      ? "green"
+                      : selectedRow?.txnType === "DEBIT"
+                      ? "red"
+                      : "orange"
+                  }
+                  fontWeight={500}
+                >
+                  {selectedRow?.status === "CREDIT"
+                    ? "PAID"
+                    : selectedRow?.status === "DEBIT"
+                    ? "NOT PAID"
+                    : selectedRow?.order?.status === "PROCESSING"
+                    ? "PENDING"
+                    : selectedRow?.order?.status}
+                </Tag>
+              </Flex>
+            )}
+            {type !== "sup_payout" && (
+              <Flex justify={"space-between"}>
+                <Text>Transaction Description</Text>
+                <Text fontWeight={500}>
+                  {selectedRow?.txnGroup === "DEBIT_ON_ORDER_CANCELLATION" ||
+                  selectedRow?.txnGroup ===
+                    "DEBIT_COMMISSION_ON_ORDER_CANCELLATION"
+                    ? "Order Cancelled"
+                    : selectedRow?.txnGroup === "CREDIT_ON_ORDER_COMPLETION"
+                    ? "Order Completed"
+                    : selectedRow?.txnGroup ===
+                      "CREDIT_COMMISSION_ON_ORDER_COMPLETION"
+                    ? "Commission On Order Completed"
+                    : selectedRow?.txnGroup === "WITHDRAW_TO_BANK"
+                    ? "Withdraw"
+                    : selectedRow?.transactionGroup === "payout"
+                    ? "Payout"
+                    : selectedRow?.transactionGroup === "repayment_interest"
+                    ? "Payment Interest"
+                    : selectedRow?.transactionGroup === "loan_interest"
+                    ? "Loan Interest"
+                    : selectedRow?.transactionGroup === "loan_disbursement"
+                    ? "Loan Disbursement"
+                    : selectedRow?.description}
+                </Text>
+              </Flex>
+            )}
+            {orderId && (
+              <Flex justify={"space-between"}>
+                <Text>OrderID</Text>
+                <Text fontWeight={500}>
+                  {/* {type === "sup_payout" ? (selectedRow?.id ?? "N/A") : type === "loan-wallet" ? (selectedRow?.identifier ?? "N/A") : (selectedRow?.order?.identifier ?? "N/A")} */}
+                  {orderId}
+                </Text>
+              </Flex>
             )}
             <Flex justify={"space-between"}>
               <Text>Tenmg Commission</Text>
