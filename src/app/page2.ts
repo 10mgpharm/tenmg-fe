8aa9142c6ea932967f3e5b3ec1849c9a23c32f9b
Element27 <olasocials@gmail.com>
2025-06-30 08:44:35 +0100

    feat: responsive pagination

---
+++ src/app/page2.ts
@@ -0,0 +1,95 @@
+const page2 = {
+  currentPage: 9,
+
+  firstPageUrl:
+    "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=1",
+  from: 241,
+  lastPage: 16,
+  lastPageUrl:
+    "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=16",
+  links: [
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=8",
+      label: "&laquo; Previous",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=1",
+      label: "1",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=2",
+      label: "2",
+      active: false,
+    },
+    {
+      url: null,
+      label: "...",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=6",
+      label: "6",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=7",
+      label: "7",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=8",
+      label: "8",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=9",
+      label: "9",
+      active: true,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=10",
+      label: "10",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=11",
+      label: "11",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=12",
+      label: "12",
+      active: false,
+    },
+    {
+      url: null,
+      label: "...",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=15",
+      label: "15",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=16",
+      label: "16",
+      active: false,
+    },
+    {
+      url: "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=10",
+      label: "Next &raquo;",
+      active: false,
+    },
+  ],
+  nextPageUrl:
+    "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=10",
+  path: "https://staging-api.10mg.ai/api/v1/account/notifications",
+  perPage: 30,
+  prevPageUrl:
+    "https://staging-api.10mg.ai/api/v1/account/notifications?limit=10&page=8",
+  to: 270,
+  total: 468,
+};
